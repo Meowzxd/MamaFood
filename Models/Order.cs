7354f54fa0ebf55e084c6267abf16d9c77670e56
Keith <shuenhong@gmail.com>
2021-03-23 00:51:11 +0800

    Added Class

---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MamaFood.Models
+{
+    public class Order: TableEntity
+    {
+        public List<OrderItem> Items { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MamaFood.Models
+{
+    public class OrderItem
+    {
+        public OrderItem(string id, int qty = 1)
+        {
+            this.FoodID = id;
+            this.Quantity = qty;
+        }
+        public string FoodID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
